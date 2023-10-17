98f49d5294db1906f62370ba57c579c0bb9507ec
EMERSON_MONGE_H <emmonge@estudiantec.cr>
2023-10-17 14:21:48 -0600

    [update] test_DB

--- source/test_DB.py
+++ source/test_DB.py
@@ -5,7 +5,7 @@ import unittest
 class TestDB(unittest.TestCase): 
     def test_IfExistFile (self):
          db = DataBase ("usuarios.txt")
-         self.assertEqual(db.Read(),-1)    
+         self.assertEqual(db.Read(),-2)    
     def test_Write(self):
         db = DataBase ("Test1.txt")
         self.assertEqual(db.EscribirArchivo("Test1"),None)  
